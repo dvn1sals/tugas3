a1ea6313bf2db80260a67b4dbb2e2cf7d2e2a632
dvn1sals <divanisalsabila07@gmail.com>
2023-11-02 10:47:49 +0700

    first commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+        //penambahan
+
+
+                        int result2 = 3 2 + ;
+                        System.out.println("Hasil penambahan: " + result2);
+
+
+
+        //pengurangan
+            int result = 4 - 3;
+            System.out.println("Hasil pengurangan: " + result);
+
+            //pembagian
+
+                double result = 10 /2 ;
+                System.out.println("Hasil pembagian: " + result);
+
+                //
+
+
+
+    }
+
+}
